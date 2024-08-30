217ec23903062f94d252a9ba29a78f4a39f2c4b3
Abdulmuhaimin Ali <abdulali115h@gmail.com>
2024-08-30 00:09:53 -0400

    Build a responsive layout

--- GameHub/src/App.tsx
+++ GameHub/src/App.tsx
@@ -1,17 +1,33 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { Button, ButtonGroup, Stack } from "@chakra-ui/react";
+import { Button, ButtonGroup, Stack, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import { Grid } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Stack spacing={4} direction="row" align="center">
-        <Button colorScheme="blue">Button</Button>
-      </Stack>
-    </div>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav main" "nav aside"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
