69972e0c5539e905b0220374dc29f62793ef0b43
Abdulmuhaimin Ali <abdulali115h@gmail.com>
2024-09-08 19:48:42 -0400

    added Abort Controller to hook

--- GameHub/src/components/GameGrid.tsx
+++ GameHub/src/components/GameGrid.tsx
@@ -1,32 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
+
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((response) => {
-        setGames(response.data.results);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
